dea5dcf5f4173b4ecc5262b96b94c25329ede188
Jefferson Oliveira <jefferoidtex7@gmail.com>
2022-07-20 02:46:43 -0300

    Cria Conexao com o Banco de Dados // Cria Migrations e Seeders Juntamente com o Endpoint para testar o Retorno do Banco de Dados

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { Candidate } from './models/candidate';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -6,6 +7,12 @@ const app    = express();
 const router = express.Router();
 
 router.get( '/', (req, res) => res.json({ hello: 'Hello World!'}) );
+
+router.get('/candidates', async (req, res) => {
+    const candidates = await Candidate.findAll()
+    return res.json(candidates)
+})
+
 
 app.use(router);
 
---
+++ src/database/index.ts
@@ -0,0 +1,9 @@
+import { Sequelize } from "sequelize";
+require('dotenv').config();
+
+const databaseUrl = process.env.DATABASE_URL || '';
+export const sequelize = new Sequelize(databaseUrl, {
+    define: {
+        underscored: true
+    }
+})
