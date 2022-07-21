22571f8acb1585978e93b080d794ea2edd0b5e4e
Jefferson Oliveira <jefferoidtex7@gmail.com>
2022-07-20 23:55:41 -0300

    Separa Rotas da app.ts para router.ts

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,14 @@
-import { Candidate } from './models/candidate';
 import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
-const app    = express();
-const router = express.Router();
+const app = express();
 
-router.get( '/', (req, res) => res.json({ hello: 'Hello World!'}) );
-
-router.get('/candidates', async (req, res) => {
-    const candidates = await Candidate.findAll()
-    return res.json(candidates)
-})
-
-
+import {router} from './routes';
 app.use(router);
 
 const PORT = process.env.PORT || 1000;
 
 app.listen(PORT, () => {
     console.log('Started!')
-})
+});
---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { Candidate } from './models/candidate';
+const router = express.Router();
+
+router.get( '/', (req, res) => res.json({ hello: 'Hello World!'}) );
+
+router.get('/candidates', async (req, res) => {
+    const candidates = await Candidate.findAll()
+    return res.json(candidates)
+})
+
+export {router}
