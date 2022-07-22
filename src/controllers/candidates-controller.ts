0cc48d274db2711547b573587523a36e3c76ae46
Jefferson Oliveira <jefferoidtex7@gmail.com>
2022-07-22 02:20:04 -0300

    Cria Demais Rotas do CRUD e Finaliza Controller de Candidatos e seu CRUD

--- src/controllers/candidates-controller.ts
+++ src/controllers/candidates-controller.ts
@@ -25,5 +25,62 @@ export const candidatesController = {
                 return res.status(400).json({ message: err.message });
             }
         }
-    }
+    },
+
+    show: async (req: Request, res: Response) => {
+        const { id } = req.params;
+
+        try {
+            const candidate = await Candidate.findByPk(id);
+
+            if(candidate === null) {
+                return res.status(400).json('Candidato não existe ' + id);
+            }
+
+            return res.status(200).json(candidate);
+        } catch (err) {
+            if(err instanceof Error) {
+                return res.status(400).json({ message: err.message });
+            }
+        }
+    },
+
+    update: async (req: Request, res: Response) => {
+        const { name, bio, email,phone,openToWork } = req.body;
+        const { id } = req.params;
+
+        try {
+            const candidate = await Candidate.findByPk(id);
+            
+            if (candidate === null) {
+                return res.status(404).json({ message: 'Candidato não encontrado' });
+            }
+
+            candidate.name = name;
+            candidate.bio = bio;
+            candidate.email = email;
+            candidate.phone = phone;
+            candidate.openToWork = openToWork;
+            candidate.save();
+
+            return res.status(200).json(candidate);
+        } catch (err) {
+            if(err instanceof Error) {
+                return res.status(400).json({ message: err.message });
+            }
+        }
+    },
+
+    delete: async (req: Request, res: Response) => {
+        const { id } = req.params;
+
+        try {
+            await Candidate.destroy({ where: { id } })
+            return res.status(204).json({message: 'Candidato deletado'});
+        } catch (err) {
+            if(err instanceof Error) {
+                return res.status(400).json({ message: err.message });
+            }
+        }
+    },
 }
--- src/routes.ts
+++ src/routes.ts
@@ -4,8 +4,10 @@ const router = express.Router();
 
 router.get( '/', (req, res) => res.json({ hello: 'Hello World!'}) );
 
-router.get('/candidates', candidatesController.index)
-
-router.post('/candidates', candidatesController.save)
+router.get('/candidates', candidatesController.index);
+router.get('/candidates/:id', candidatesController.show);
+router.post('/candidates', candidatesController.create);
+router.put('/candidates/:id', candidatesController.update);
+router.delete('/candidates/:id', candidatesController.delete);
 
 export {router}
