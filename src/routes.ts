025c3c148362ef20462f434d282d7db976ee5178
Jefferson Oliveira <jefferoidtex7@gmail.com>
2022-07-24 18:45:18 -0300

    Cria CRUD para Companies e Seus Endpoints

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import { candidatesController } from './controllers/candidates-controller';
+import { companiesController } from './controllers/companies-controller';
+
 import express from 'express';
 const router = express.Router();
 
@@ -9,5 +11,11 @@ router.get('/candidates/:id', candidatesController.show);
 router.post('/candidates', candidatesController.create);
 router.put('/candidates/:id', candidatesController.update);
 router.delete('/candidates/:id', candidatesController.delete);
+
+router.get('/companies', companiesController.index)
+router.get('/companies/:id', companiesController.show)
+router.post('/companies', companiesController.create)
+router.put('/companies/:id', companiesController.update)
+router.delete('/companies/:id', companiesController.delete)
 
 export {router}
