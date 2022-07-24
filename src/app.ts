99d9187fdf81e1f434125bfe899ca7ca467ae5fe
Jefferson Oliveira <jefferoidtex7@gmail.com>
2022-07-24 18:02:42 -0300

    Configura Swagger e Já configura Nela o Endpoint Candidates

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,16 @@ dotenv.config();
 import express from 'express';
 const app = express();
 
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json'
+
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));     
+
 import {router} from './routes';
 app.use(express.json());
 app.use(router);
 
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 2000;
 
 app.listen(PORT, () => {
     console.log('Started!')
